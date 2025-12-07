45f05ed7883a1e7f0c8f839cc8c743addfcb30e8
agustinment <institucionalagustinmendezt@gmail.com>
2025-12-07 10:28:50 -0300

    habits states

--- src/components/HabitTracker.jsx
+++ src/components/HabitTracker.jsx
@@ -1,3 +1,55 @@
 // STATEFUL COMPONENT
 import React, { useState } from 'react';
 import '../styles/HabitTracker'
+
+const HabitTracker = () => {
+    const [habits, setHabits] = useState([
+        {
+            name: 'Practice React',
+            weekState: {
+                Mo: false, 
+                Tu: false, 
+                We: false, 
+                Th: false, 
+                Fr: false, 
+                Sa: false, 
+                Su: false
+            }
+        },
+        {
+             name: 'Make my bed',
+            weekState: {
+                Mo: false, 
+                Tu: false, 
+                We: false, 
+                Th: false, 
+                Fr: false, 
+                Sa: false, 
+                Su: false
+            }
+        },
+        {
+            name: 'Do exercise',
+            weekState: {
+                Mo: false, 
+                Tu: false, 
+                We: false, 
+                Th: false, 
+                Fr: false, 
+                Sa: false, 
+                Su: false
+            }
+        }
+
+    ]);
+
+    return (
+        <>
+        </>
+    )
+
+};
+
+
+
+export default HabitTracker;
